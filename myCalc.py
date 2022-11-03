2b07351a77dbd3149eca8f65f49257f8055ecb45
sangah <tkddk5009@naver.com>
2022-11-03 15:33:49 +0900

    프로젝트 관리 시작(커밋1) 첫 커밋을 진행함

---
+++ myCalc.py
@@ -0,0 +1,36 @@
+## 함수 선언부
+def add_func(n1, n2) :
+    return n1+n2
+
+def sub_func(n1, n2) :
+    return n1-n2
+
+def mul_func(n1, n2) :
+    return n1*n2
+
+def div_func(n1, n2) :
+    return n1/n2    
+
+## 전역 변수부
+num1=100
+num2=200
+res=0
+
+## 메인 코드부
+
+res=add_func(num1,num2)
+
+print(num1, "+", num2, "=", res)
+
+
+res=sub_func(num1, num2)
+
+print(num1, "-", num2, "=", res)
+
+res=mul_func(num1, num2)
+
+print(num1, "*", num2, "=", res)
+
+res=div_func(num1, num2)
+
+print(num1, "/", num2, "=", res)
